f577ba46e97230762a5a39164a3ee8435cc13179
MoeadRehan <moeadrehan124125@gmail.com>
2024-01-17 14:41:26 +0500

    Filter File

--- src/higherOrderFunctions/filter.js
+++ src/higherOrderFunctions/filter.js
@@ -1,3 +1,8 @@
 console.log("################ filter Method ################");
+////All students living in New York
 const peopleInNewYork = people.filter((person) => person.location && person.location.city === "New York").map((person) => person.name);
 console.log(peopleInNewYork);
+
+//All Senior citizens (age > 45)
+const peoplesSeniorCitizens = people.filter((person) => person.age > 45).map((person)=>person.name);
+conole.log(peoplesSeniorCitizens);
